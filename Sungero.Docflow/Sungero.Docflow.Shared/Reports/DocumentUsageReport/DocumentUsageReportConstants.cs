8279babec07174fb0261c6511798fbf906d2ad6a
DMITRIY STARKOV <92165948+DmitriyItProject16@users.noreply.github.com>
2024-02-12 09:39:42 +0700

    RX 4.6.39

---
+++ Sungero.Docflow/Sungero.Docflow.Shared/Reports/DocumentUsageReport/DocumentUsageReportConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sungero.Docflow.Constants
+{
+  public static class DocumentUsageReport
+  {
+    // Код диалога.
+    public const string HelpCode = "Sungero_Docflow_DocumentUsageReportDialog";
+    
+    // Имя таблицы для выборки сотрудников.
+    public const string EmployeeTableName = "Sungero_Reports_DocumentUsageReport_Employees";
+    
+    // Имя таблицы для выборки истории по документам в разрезе сотрудников.
+    public const string HistoryTableName = "Sungero_Reports_DocumentUsageReport_History";    
+  }
+}
